dc426037ac3c3f1fd2b8e7aca37d50903957cfa8
Anderesu44 <141999897+Anderesu44@users.noreply.github.com>
2024-10-15 00:19:40 -0400

    v: 2.5 terminada solo mejoras en las unsiones y estabilidad

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 __author__ = "A44"
-__version__ = 2.0
+__version__ = 2.5
 
-from os import getcwd, mkdir, path, system
-from shutil import move
+
+from os import getcwd, listdir, mkdir, path, system
 from sys import argv,exit
 from typing import NoReturn
 
 from modules import ID_ERR as ID_ERROR
-from modules.A44M import ConfigManager, format_text
+from A44M import ConfigManager, format_text
 from modules.Entities import Animes, Reception
 
 
@@ -17,40 +17,46 @@ SINTAX_ERROR ="the command syntax's not correct try --help"
 COMMAND_UNKNOWN = "\ncommand unknown\ntry -h or --help\n"
 ACCESS_DENIED = "Access denied"
 
-VERSION = "2.0.0"
-CONFIG_DIR = "C:\\ProgramData\\Anderesu44\\animes"
+VERSION = "2.5.0"
+CONFIG_DIR = path.expanduser("~\\animes")
+INST_DIR = "C:\\Program Files\\Anderesu44\\animes"
 INIT_CONFIG = {"DEFAULT_ANIMES_DIR": "~/Videos/Animes","DEFAULT_RECEPTION_DIR": "~/Videos/Animes/0000","DEFAULT_ICONS_FOLDER":None,"ANIMES_DIR": "~/Videos/Animes","RECEPTION_DIR": "~/Videos/Animes/0000","ICONS_FOLDER":None,"ANIMES_OLD_DIR": "~/Videos/Animes","RECEPTION_OLD_DIR": "~/Videos/Animes/0000","ICONS_OLD_FOLDER":None}
 CFG = ConfigManager(CONFIG_DIR,init=INIT_CONFIG)
 ANIMES_DIR = path.realpath(path.expanduser(CFG["ANIMES_DIR"]))
 RECEPTION_DIR = path.realpath(path.expanduser(CFG["RECEPTION_DIR"]))
-
-def main(*args: str,**kwargs):
+int
+def main(*args:tuple[str]): 
     HELP="""
 animes <command> [option]
-    -n or --new     => make new animme folder
-    -s or --sort    => sort the animes in reseption folder
-    --config        => changes the program configuration
+    -n or --new      => make new animme folder
+    -s or --sort     => sort the animes in reception folder
+    -u or --update   => update the database or the app if needed
+    --config         => changes the program configuration
 
-    -v or --view    => show all animes folders
-    -h or --help    => show help
-    --version       => show vesion
+    -d or --download => show the downloads links and copy them on the clipboard
+    -v or --view     => show all animes folders
+    -h or --help     => show help
+    --version        => show vesion
 
 special commands
     --wait          => the program waits before ending
 """
     if len(args) == 1:
         exit(HELP)
-    match args[1]:
+    match args[1].lower():
         case "-n"|"--new":
             _new(*args[1:])
         case "-s"|"--sort":
             _sort(*args[1:])
+        case "-u"|"--update":
+            exit("in development")
         case "--config":
             _config(*args[1:])
-            pass
+        case "-d"|"--download":
+            exit("in development")
+            _download(*args[1:])
         case "-v"|"--view":
             _view(args[1:])
-            pass
         case "-h"|"--help":
             print(HELP)
             if "--wait" in args:
@@ -61,15 +67,16 @@ special commands
             if "--wait" in args:
                 input()
             exit()
+        case "--show-ins_dir":
+            exit(INST_DIR)
         case _:
             exit(COMMAND_UNKNOWN)
-
 
 def _new(*args)->NoReturn:
     HELP = f"""
 animes {args[0]} <id:int> <name:str>
-
 all the arguments after the 3rd will be taken as a name
+animes {args[0]} <id:int> <--auto>
 """
     if len(args) == 1 or args[1] == "-h":
         exit(HELP)
@@ -81,11 +88,17 @@ all the arguments after the 3rd will be taken as a name
         num = f"{int(num):04}"
         if len(str(num)) != 4:
             exit(ID_ERROR)
+    if args[2] == "--auto":
+        animes = Animes(ANIMES_DIR)
+        try:
+            animes.add_anime(str(num),CFG["ICONS_FOLDER"])
+        except KeyError as Error:
+            exit(f'{str(Error).strip("'")}\npuede solusionar el problema de las siguientes maneras:\ncompruebe que el id, debe dar una id valida no un nombre\ncompruebe la version de la vase de datos si es necesario actualice\nEn caso de que el error persista comuniquese con A44')
+        exit("All right")
     nam = ""
     for a in args[2:]:
             nam += a+" " 
     nam = nam.strip(" ")
-    
     if not _valid_name(nam):
         exit(NAME_ERROR)
     
@@ -111,6 +124,7 @@ all the arguments after the 3rd will be taken as a name
     exit("All right")
 
 def _sort(*args)->NoReturn:
+    from shutil import move,Error as MoveError
     HELP = f"""
 animes <{args[0]}>
 animes <{args[0]}> [path]
@@ -127,6 +141,9 @@ special commands
     animes_path = ANIMES_DIR
     if "--wait" in args:
         wait = True
+        temp = list(args)
+        temp.remove("--wait")
+        args = tuple(temp)
     if len(args) == 1:
         reception_path = RECEPTION_DIR
     else:
@@ -151,24 +168,57 @@ special commands
                 else:
                     if not path.exists(args[3]):
                         exit(f'"{args[3]}" Not Found or not a folder')
-                    animes_path = path.realpath(path. expanduser(args[3]))
+                    animes_path = path.realpath(path.expanduser(args[3]))
             else:
                 exit(COMMAND_UNKNOWN)
     
     animes = Animes(animes_path)
     reception = Reception(reception_path)
     dessorted_caps = reception.get_caps()
-    iter = -1
+    
+    progres = 0
+    end = len(dessorted_caps)
     for cap in dessorted_caps:
+        porsent = 0
+        print(f'\r{("▬"*int(int(porsent)//2)) .ljust(50,"-")}',end=f" {format_text((lambda a: str(int(float(a))) if a.split(".")[-1] == "0" else a)((format_text(porsent,three_dot="",max=5))),"%",sep="",min_fill=(6," "))}/100% | {progres}/{end}")
         try:
             anime = animes.get_anime_by_id(cap.id)
         except KeyError:
-            anime = animes.add_anime(f"{cap.id}_unamed")
-        move(cap.path,anime.path)
-        if iter == 0:
-            print(f"relocated files:")
-        print(f"\t{cap} to {anime.name}")
-    print("All Sorted")
+            anime = animes.add_anime(f"{cap.id}",CFG["ICONS_FOLDER"])
+        try:
+            move(cap.path,anime.path)
+        except MoveError as Error:
+            Error = str(Error)
+            print(f'\nDestination "{cap}" already exists')
+        except OSError as Error:
+            Error = str(Error)
+            exit("\n"+Error.replace(format_text(*Error[Error.find("["):Error.find("]")+1],sep=""),""))
+        # except FileExistsError:
+            # pass
+        progres+=1
+        porsent = progres/end*100
+        print(f'\r{("▬"*int(int(porsent)//2)) .ljust(50,"-")}',end=f" {format_text((lambda a: str(int(float(a))) if a.split(".")[-1] == "0" else a)((format_text(porsent,three_dot="",max=5))),"%",sep="",min_fill=(6," "))}/100% |{progres}/{end}")
+    if len(dessorted_caps)>0:
+        print(f"\r{"▬"*50} 100%                \nCaps sorted",end="\n")
+    
+    dessorted_icos = reception.get_caps()
+    progres = 0
+    end = len(dessorted_icos)
+    for ico in dessorted_icos:
+        try:
+            anime = animes.get_anime_by_id(ico.id)
+        except KeyError:
+            anime = animes.add_anime(f"{ico.id}",CFG["ICONS_FOLDER"])
+        try:
+            move(ico.path,anime.path)
+        except MoveError:
+            pass
+        progres+=1
+        porsent = progres/end*100
+        print(f'\r{("▬"*int(int(porsent)//2)) .ljust(50,"-")}',end=f" {format_text((lambda a: str(int(float(a))) if a.split(".")[-1] == "0" else a)((format_text(porsent,three_dot="",max=5))),"%",sep="",min_fill=(6," "))}/100%")
+    if len(dessorted_icos) > 1:
+        print(f"\r{"▬"*50} 100%                \nIcons sorted",end="\n")
+    print("All Sorted",end="")
     if wait:
         input()
     exit()
@@ -182,6 +232,8 @@ def _view(*args)->NoReturn:
     exit()
 
 def _config(*args)->NoReturn:
+    from time import sleep
+    from keyboard import KEY_DOWN, read_event
     HELP = f"""
 animes {args[0]} <commands> [options]
 
@@ -210,7 +262,29 @@ commands
         case "-h":
             exit(HELP)
         case "--cfg"|"-o":
-            system(path.join(CONFIG_DIR,"a44.json"))
+            editor = "notepad"
+            print("abrir con:\n>Notepad  VSCode",end="")
+            screen = ">Notepad  VSCode"
+            while True:
+                event = read_event()    
+                if event.name == "esc":
+                    exit("cancel")
+                if event.event_type == KEY_DOWN:
+                    if event.name == "left":
+                        screen = "\r>Notepad  VSCode"
+                        editor = "notepad"
+                    if event.name == "right":
+                        screen = "\r Notepad >VSCode"
+                        editor = "code"
+                    if event.name == "enter":
+                        print("\nopen config file.",end="")
+                        sleep(1)
+                        print("\ropen config file..",end="")
+                        sleep(1)
+                        system(f'{editor} "{path.join(CONFIG_DIR,"cfg.json")}"')
+                        exit("\ropen config file...")
+                print(screen,end="")
+
         case "--a_dir"| "--r_dir"|"--dir":
             if len(args) == 2:
                 exit(HELP_1)
@@ -236,9 +310,9 @@ commands
                 case "-h":
                     exit(HELP_1)
                 case "--back" | "-b":
-                    path_2 = path.realpath(path.expanduser(CFG["ICONS_OLD_FOLDER"]))
+                    path_2:str|None = (lambda a:path.realpath(path.expanduser(a)) if a else None)(CFG["ICONS_OLD_FOLDER"])
                 case "--default" | "-d":
-                    path_2 =path.realpath(path.expanduser( CFG["DEFAULT_ICONS_FOLDER"]))
+                    path_2:str|None = (lambda a:path.realpath(path.expanduser(a)) if a else None)(CFG["DEFAULT_ICONS_FOLDER"])
                 case "none"|"null"|"false":
                     path_2 = None
                 case _:
@@ -271,19 +345,21 @@ commands
                 exit(ACCESS_DENIED)
             except FileExistsError:
                 pass
-            CFG["RESEPTION_DIR"] = path_1
+            CFG["RECEPTION_DIR"] = path_1
             CFG["RECEPTION_OLD_DIR"] = RECEPTION_DIR
             CFG.save_config()
         case "--i_dir":
             try:            
                 if path_2:
                     mkdir(path_2)
+                    path_2 = path_2.replace(path.expanduser("~"),"~")
             except FileNotFoundError:
                 exit(DIR_ERROR)
             except PermissionError:
                 exit(ACCESS_DENIED)
             except FileExistsError:
                 pass
+            
             CFG["ICONS_OLD_FOLDER"] = CFG["ICONS_FOLDER"]
             CFG["ICONS_FOLDER"] = path_2
             CFG.save_config()
@@ -295,6 +371,7 @@ commands
                 animes = Animes(ANIMES_DIR)
                 for anime in animes.animes:
                     anime.set_icon(path_2)
+                exit('Successful')
         case "--dir":
             try:
                 mkdir(path_0)
@@ -308,24 +385,45 @@ commands
             CFG["ANIMES_DIR"] = path_0 
             CFG["ANIMES_OLD_DIR"] = ANIMES_DIR
             CFG["RECEPTION_DIR"] = path.join(path_1,"0000")
-            CFG["RESEPTION_OLD_DIR"] =  RECEPTION_DIR
+            CFG["RECEPTION_OLD_DIR"] =  RECEPTION_DIR
             CFG.save_config()
         case _:
             exit(COMMAND_UNKNOWN)
     exit("Successful")
+   
+def _download(*agrs)->NoReturn:
+    HELP = """
+    """
+    exit()
     
 def _valid_name(name:str)->bool:
     if "\\" in name or "/" in name or ":" in name or "*" in name or "?" in name or '"' in name or "<" in name or ">" in name or "|" in name:
         return False
     else:
         return True
-def _back_dir()->str:
-    _dir = getcwd()
-    temp = _dir.split("\\")
-    temp.pop()
-    _dir = ""
-    for folder in temp:
-        _dir += folder + "\\"
-    return _dir
+
 if __name__ == '__main__':
-    main(*argv)
+    try:
+        # main(*argv)
+        main("animes","-s")
+    except Exception as Error:
+        #!pide permiso
+        from json import dumps
+        from datetime import datetime
+        from sys import platform
+        file = path.join(INST_DIR,"feed.txt")
+        txt = f"""{datetime.now()}:\n{format_text(*argv)}\r	  {type(Error).__name__}: {str(Error)}:
+            version:{VERSION}
+            cfg:{"{"}
+{dumps(CFG,indent=18).rstrip("\n}}").lstrip("\n{{")}
+            {"}"}
+            sys: {platform}
+            cfgdir: {CONFIG_DIR}
+            anidir: {ANIMES_DIR}
+            recdir: {RECEPTION_DIR}
+            insdir: {INST_DIR}
+            Conten: {format_text(*listdir(INST_DIR),sep="\n                    ")} 
+            \n"""
+        with open(file,"a") as f:
+            f.write(txt)
+        exit("\nups a ocurrido un error inesperado\nPorfavor contacte con a44 en:\nhttps://anderesu44.github.io/descktop/#/msg")
--- modules/Entities.py
+++ modules/Entities.py
@@ -1,8 +1,56 @@
+from operator import truediv
 from os import listdir, mkdir, path, system
-
 from modules import ID_ERROR,FormatError,ID_ERR
-from modules.A44M import A44Map, format_text
+from A44M import A44Map,ConfigManager ,format_text
+from modules.hocks import format_name_to_dir
+CONFIG_DIR = path.expanduser("~\\animes")#!".\\database"
+ANIMES = ConfigManager(CONFIG_DIR,"animes.json")
 
+class Ico():
+    def __init__(self,_path:str):
+        if not path.isfile(_path):
+            raise FileNotFoundError(f'"{_path}" Not Found or not a file')
+        _path = path.realpath(_path)
+        
+        localitation = path.dirname(_path)
+        file = path.basename(_path)
+        extension = file.split(".")[-1]
+        if not extension in ["jpeg", "jpg", "png", "gif","webp", "ico", "bmp"]:
+            raise FormatError(f'"{_path}" Not is a image')
+        id = self.__get_id(file)
+
+        if extension == "ico":
+            status = True
+        else:
+            status = False
+
+        self.id:int = id 
+        self.path:str = _path
+        self.location:str = localitation
+        self.extension:str = extension
+        self.formatted:bool = status
+    def __str__(self)->str:
+        return f"{self.id}_{self.name}.mp4"
+    
+    def __get_id(self,file:str)->int:
+        _id = file.split("_")[0]
+        if len(_id) > 4 or len(_id) < 3 or _id == "0000":
+            raise ID_ERROR(ID_ERR+f'"{_id}"')
+        try:
+            _id = int(_id)
+        except ValueError:
+            raise  ID_ERROR(ID_ERR+f'"{_id}"')
+        return _id
+    def get_staus(self)->str:
+        if self.formatted:
+            return "formatted"
+        else:
+            return "uncformatted"
+    def converting(self)-> bool:
+        #?script convertidor
+        #?script eliminador
+        pass
+        
 class Cap():
     def __init__(self,_path:str):
         if not path.isfile(_path):
@@ -78,11 +126,12 @@ class Anime():
             raise  ID_ERROR(ID_ERR+f'"{_id}"')
         return _id
     def __str__(self):
-        return f"{self.name}: {self.length}"
-    def set_icon(self,icons_path):
+        return f"{format_text(self.name,max=35)}: {self.length}"
+    def set_icon(self,icons_path: str|None=None):
+        
         init_file_path = path.join(self.path,"desktop.ini")
         if icons_path:
-            icon_path = path.join(icons_path,f"{self.id}.ico")
+            icon_path = path.join(path.expanduser(icons_path),f"{self.id}.ico")
         else:
             icon_path = f"{self.id}.ico"
         iter = 0
@@ -123,19 +172,25 @@ class Animes():
         self.path = _path
         self.animes:tuple[Anime]=tuple(animes)
     def get_animes(self):
-        pass
+        return list(self.animes)
     def get_anime_by_id(self,id:int)->Anime:
         for anime in self.animes:
             if anime.id == id:
                 return anime
         raise KeyError(id)
-    def add_anime(self,anime:str):
+    def add_anime(self,anime_id:str,icons_path:str|None=None):
         try:
-            mkdir(path.join(self.path,anime))
+            anime_name = format_name_to_dir(ANIMES[anime_id]["title"])
+        except KeyError as Error:
+            raise KeyError(f'El id: "{str(Error).strip("'")}" no se encuentra en la base de datos')
+        anime_path = f"{anime_id}_{anime_name}"
+        try:
+            mkdir(path.join(self.path,anime_path))
         except FileExistsError:
             pass    
         try:
-            anime = Anime(path.join(self.path,anime))            
+            anime = Anime(path.join(self.path,anime_path))
+            anime.set_icon(icons_path)
         except ID_ERROR:
             anime = None
         except FileNotFoundError:
@@ -146,8 +201,10 @@ class Animes():
             self.animes = tuple(animes)
         return anime
     def __str__(self)->str:
-        str_ = (lambda a:f"{a} Animes:\n" if a > 0 else f"No Hay animes en el directorio especificado\n:    {self.path}\nanimes:    {self.animes}")(self.length)
-        for anime in self.animes:
+        str_ = (lambda a:f"{a} Animes:\n" if a > 0 else f"No Hay animes en el directorio especificado")(self.length)
+        animes = list(self.animes)
+        animes.reverse()
+        for anime in animes:
             str_ += f"{anime}\n"
         return str_
 class Reception():
@@ -157,15 +214,26 @@ class Reception():
             raise FileNotFoundError(f'"{_path}" Not Found')
         self.map = A44Map(path_=_path)
         caps:list = []
+        icos:list = []
         def temp(_path):
             try:
                 caps.append(Cap(_path))
+                return
             except ID_ERROR:
                 pass
             except FormatError:
                 pass
+            try:
+                icos.append(Ico(_path))
+                return
+            except ID_ERROR:
+                pass
+            except FormatError:
+                pass
+                
         self.map._map(files_function=(temp))
         self.caps:tuple[Cap] = tuple(caps)
+        self.icos:tuple[Ico] = tuple(icos)
     def get_caps_path(self)-> list[str]:
         caps_p = []
         for cap in self.caps:
@@ -173,4 +241,6 @@ class Reception():
         return caps_p
         
     def get_caps(self)->list[Cap]:
-        return tuple(self.caps)
+        return list(self.caps)
+    def get_icos(self)->list[Ico]:
+        return list(self.icos)
